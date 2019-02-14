e11913a565e2d9839d354b14f43a90e0f941fee6
Sivaprasad <Mean563>
2019-02-14 23:22:36 +0530

    create Spring Boot Project with JPA and Mysql

---
+++ b2bapp/src/main/java/com/volksevis/b2bapp/B2bappApplication.java
@@ -0,0 +1,14 @@
+package com.volksevis.b2bapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2bappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2bappApplication.class, args);
+	}
+
+}
+
